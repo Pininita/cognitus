41857475d52c9f9f52aab4216cf10bd5b349ff9f
Ronal Taskhit <santiagorojas772@gmail.com>
2025-07-06 12:01:19 -0500

    correcion y ejecucion de ejercicio 1 y 2.

--- Calcaulator.js
+++ Calcaulator.js
@@ -2,39 +2,43 @@
 validar qus no se divida por 0
 mostrar mensaje de error
 redondear los resultados a 2 decimales*/
-
+//numero redondeado vacio
 let n4 = '';
+//fubcion operaciones vbasicas
 function cal(n1, n2, op) {
-
+//valida si la operacion no es un simbolo
     if (typeof op != 'number') {
+        //opciones si  es un simbolo continuar con los casos
     switch (op) {
+        //suma
         case "+":
        n3= n1+n2; 
        n4 = n3.toFixed(2)
-console.log(n4);
-              
-            
-            break;
-
+      console.log(n4);
+    break;
+//resta
         case "-":
- 
-           n3= n1 - n2; 
+            n3= n1 - n2; 
             n4 = n3.toFixed(2)
-console.log(n4);
+         console.log(n4);
          break;
-        
+        //multiplicacion
+
          case "*":
            n3= n1*n2; 
        n4 = n3.toFixed(2);
     console.log(n4);
          break;
-        
+        //division
          case "/":
+            //si n2 es igual  a 0
             if (n2 == 0) {
                 console.log("This number not divisible for 0");
                 
                 
-            } else {
+            } 
+            // si no es igual a 0
+            else {
         n3= n1/n2; 
         n4 = n3.toFixed(2);
         console.log(n4);                
@@ -42,17 +46,18 @@ console.log(n4);
     
                 
             break;
+            //si no entras los numeros de la operacion
         default:
             console.log("please enter numbers for the operation's")
             
             break;
     }
   
-    
+    //si el operador es un numero
 } else {
  console.log("this data are not operation symbol");
  
 }
 }
-
+//llamado funcion
 cal('+');
---
+++ password.js
@@ -0,0 +1,44 @@
+/*Crea una funcion  que valide si una contraseña es segura segun los critarios de la seguridad bsncarias
+
+minimo 8 caracteres
+almenos una lretra mayuscula.
+al menos una minuscula*
+al menos un numero.
+al me os un caracyers especial*/
+
+//fyubcion saber si contraseña es segura .
+function pass(p) {
+// vaida si la contraseña es corta.
+     if(p.length <=8) {
+          return ("necesitas una contraseña larga");
+     }
+    // vaida si hay letras minusculas.
+
+if  (!/[a-z]/.test(p)) {
+            return ("la contraseña no tiene letras minusculas");
+}  
+     // vaida si hay letras mayusculas.
+ if (!/[A-Z]/.test(p)) {
+            return("la contraseña no tiene letras mayusculas");
+}
+
+// vaida si hay numeros.
+
+    if (!/[\d]/.test(p)) {
+       return("la contraseña no tiene numeros");
+            }
+// vaida si hay caracteres especiales.
+
+    if (/^[a-zA-Z0-9]*$/.test(p)){
+    return ("la contraseña no caracteres especiales");
+    }  
+    // vaida si hay contraseña correcta.
+{
+    return("contraseña creada exitosamente y es segura")
+    }
+        
+}
+
+//llaado funcion en console log
+
+     console.log(pass("lioneKKKKK12&"));
